e361882a3a67a2d0bd1dd6874d463c3ee44d46d0
Dennis A. Boanini <dennis.boanini01@gmail.com>
2022-06-25 18:27:39 +0200

    Finished implementing What GPT-3 section

--- src/components/feature/Feature.tsx
+++ src/components/feature/Feature.tsx
@@ -1,13 +1,21 @@
 // @flow
 import * as React from 'react';
+import './feature.scss';
 
 type Props = {
-
+    title: string;
+    text: string;
 };
-export const Feature = (props: Props) => {
+export const Feature = ({ title, text }: Props) => {
     return (
-        <div>
-            Feature
+        <div className="gpt3__features-container__feature">
+            <div className="gpt3__features-container__feature-title">
+                <div />
+                <h1>{title}</h1>
+            </div>
+            <div className="gpt3__features-container__feature-text">
+                <p>{text}</p>
+            </div>
         </div>
     );
 };
