c8190512f9d84589091a6972199547f06a825ea1
Dennis A. Boanini <dennis.boanini01@gmail.com>
2022-06-26 14:58:25 +0200

    Finished blog section

--- src/components/article/Article.tsx
+++ src/components/article/Article.tsx
@@ -1,13 +1,25 @@
 // @flow
 import * as React from 'react';
+import './article.scss';
 
 type Props = {
-
+    imgUrl: string;
+    date: string;
+    title: string;
 };
 export const Article = (props: Props) => {
     return (
-        <div>
-            Article
+        <div className="gpt3__blog-container_article">
+            <div className="gpt3__blog-container_article-img">
+                <img src={props.imgUrl} alt="blog"/>
+            </div>
+            <div className="gpt3__blog-container_article-content">
+                <div>
+                    <p>{props.date}</p>
+                    <h3>{props.title}</h3>
+                </div>
+                <p>Read Full Article</p>
+            </div>
         </div>
     );
 };
