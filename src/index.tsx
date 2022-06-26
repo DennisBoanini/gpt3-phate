46cb8828626cc2c7f95e3d47d75dac2f42d517a8
Dennis A. Boanini <dennis.boanini01@gmail.com>
2022-06-26 16:32:27 +0200

    Installed sass instead of node-sass that is deprecated

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './variables.scss';
+import './styles/variables.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
